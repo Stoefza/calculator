c681d2cb13c83b906551a330d8a9a6e1b5d0e4d5
Stefan <84515587+Stoefza@users.noreply.github.com>
2024-03-07 22:04:39 +0200

    Add: comments

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const numberButtons = document.querySelectorAll(".number-button");
 const clearButton = document.getElementById("clear").addEventListener("click", () => clear());
 const displayValue = document.querySelector(".display-value");
 
+// Adds an event listener to each number button and appends the number to numberInput
 numberButtons.forEach(button => {
 	button.addEventListener("click", function () {
 		if (numberInput === "" && button.textContent === "0") {
@@ -18,22 +19,28 @@ numberButtons.forEach(button => {
 	});
 });
 
+// Build function to run on operator click and 
+// set the firstNumber = numberInput
+
+
+// addition function
 function add(num1, num2) {
 	return num1 + num2;
 }
-
+//subtract function
 function subtract(num1, num2) {
 	return num1 - num2;
 }
-
+//mulitply function
 function multiply(num1, num2) {
 	return num1 * num2;
 }
-
+//devide function
 function devide(num1, num2) {
 	return num1 / num2;
 }
 
+//Switch staement to determine which operator was selected
 function operate(firstNumber, operator, secondNumber) {
 	switch (operator) {
 		case "+":
@@ -49,6 +56,7 @@ function operate(firstNumber, operator, secondNumber) {
 	}
 }
 
+//This clears the calculator
 function clear() {
 	console.log("Clearing");
 	firstNumber = "";
