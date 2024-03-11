856bdaa52ce23909bc768078c4a4aaa1693285b0
Stefan <84515587+Stoefza@users.noreply.github.com>
2024-03-11 10:16:14 +0200

    Working on logic

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let firstNumber = "";
-let secondNumber;
+let firstNumber = 0;
+let secondNumber = 0;
 let numberInput = "";
 let operator;
+let result = 0;
+let operatorClicked = 0;
 
 const numberButtons = document.querySelectorAll(".number-button");
 const clearButton = document.getElementById("clear").addEventListener("click", () => clear());
 const displayValue = document.querySelector(".display-value");
+const calcValue = document.querySelector(".calculated-display-value");
+const equalsButton = document.querySelector(".equals-button");
+
+const operationButtons = document.querySelectorAll(".operator-button");
 
 // Adds an event listener to each number button and appends the number to numberInput
 numberButtons.forEach(button => {
@@ -13,19 +19,22 @@ numberButtons.forEach(button => {
 		if (numberInput === "" && button.textContent === "0") {
 		} else {
 			numberInput += button.textContent;
-			console.log("number input:" + numberInput);
 			displayValue.textContent = numberInput;
+			firstNumber = parseInt(numberInput);
+			// console.log(`first number: ${firstNumber}`);
 		}
 	});
 });
 
-// Build function to run on operator click and 
+// Build function to run on operator click and
 // set the firstNumber = numberInput
-
 
 // addition function
 function add(num1, num2) {
-	return num1 + num2;
+	console.log(`the type of First Number ` + typeof(firstNumber))
+	console.log(`the type of Second Number ` + typeof(secondNumber))
+	let value = (num1 + num2)
+	return value;
 }
 //subtract function
 function subtract(num1, num2) {
@@ -44,6 +53,8 @@ function devide(num1, num2) {
 function operate(firstNumber, operator, secondNumber) {
 	switch (operator) {
 		case "+":
+			console.log(`the type of First Number ` + typeof(firstNumber))
+			console.log(`the type of Second Number ` + typeof(secondNumber))
 			return add(firstNumber, secondNumber);
 		case "-":
 			return subtract(firstNumber, secondNumber);
@@ -52,16 +63,94 @@ function operate(firstNumber, operator, secondNumber) {
 		case "/":
 			return devide(firstNumber, secondNumber);
 		default:
-			return "Uh Oh";
+			break;
 	}
 }
 
 //This clears the calculator
 function clear() {
 	console.log("Clearing");
-	firstNumber = "";
-	secondNumber = "";
-    numberInput = "";
+	firstNumber = 0;
+	secondNumber = 0;
+	numberInput = "";
 	operator = "";
+	result = 0;
+	operatorClicked = 0;
 	displayValue.textContent = "";
+	calcValue.textContent = ""
+
 }
+
+operationButtons.forEach(operationButton => {
+	operationButton.addEventListener("click", () => {
+		operatorClicked++;
+		console.log(operator)
+		if (operationButton.textContent !== operator){
+			operator = operationButton.textContent
+
+		} else {
+			firstNumber = operate(firstNumber,operator,firstNumber)
+			calcValue.textContent = firstNumber
+			console.log(`first number: ${firstNumber}`)
+
+			console.log(operator)
+
+		}
+		
+		// console.log(firstNumber);
+		// console.log(`opreator clicked : ${operatorClicked}`);
+		// secondNumber = firstNumber;
+		// firstNumber = 0
+		console.log(`first number: ${firstNumber}`)
+		console.log(`second number: ${secondNumber}`)
+		// calcValue.textContent = firstNumber
+		// result = operate(firstNumber,operator,secondNumber)
+		// calcValue.textContent = result
+		// calcValue.textContent = `total: ${secondNumber}`
+		// if (operatorClicked > 1){
+			
+		// 	calcValue.textContent = result
+		// 	 	result = operate(secondNumber,operator,secondNumber)
+		// 		secondNumber = result
+		// 		return result
+		// }
+		// Set second number
+		// if operator > 1 pass 2nd number twice to opariotn
+		
+	});
+});
+
+equalsButton.addEventListener("click", () => {});
+
+// operationButtons.forEach(operationButton => {
+// 	operationButton.addEventListener("click", () => {
+// 		if (operationButton.textContent !== "=" && result == 0) {
+// 			operator = operationButton.textContent;
+// 			console.log(operator);
+// 			firstNumber = numberInput;
+// 			console.log(firstNumber);
+// 			numberInput = "";
+// 			displayValue.textContent = "";
+// 			calcValue.textContent = firstNumber
+// 			result = firstNumber;
+// 		} else {
+// 			if (result == 0) {
+// 				secondNumber = numberInput;
+// 				console.log(`First number: ${firstNumber}`);
+// 				console.log(`Second number: ${secondNumber}`);
+// 				result = operate(parseInt(firstNumber), operator, parseInt(secondNumber));
+// 				calcValue.textContent = result
+// 				displayValue.textContent = '';
+// 			} else {
+// 				secondNumber = numberInput;
+// 				console.log(`First number: ${firstNumber}`);
+// 				console.log(`Second number: ${secondNumber}`);
+// 				result = operate(parseInt(result), operator, parseInt(secondNumber));
+// 				calcValue.textContent = result
+// 				displayValue.textContent = '';
+// 			}
+// 			secondNumber = "";
+// 			numberInput = "";
+// 		}
+// 	});
+// });
